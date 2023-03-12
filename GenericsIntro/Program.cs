0db18887a5af15111dfb1faaef1dd539a220af5d
mikai <mikai@MIKAIL>
2023-03-12 16:40:39 +0300

    Add project files.

---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,11 @@
+﻿using GenericsIntro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        MyList<string> myList=new MyList<string>();
+        myList.Add("Mikail");
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string kategoriEtiketi = "Kategori";
+        int ogrenciSayisi = 32000;
+
+    }
+}
